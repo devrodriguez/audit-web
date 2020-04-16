2991de230bd572a2e2491c1548740d2d0c120d8e
devrodriguez <john.rodriguez.25@hotmail.com>
2020-04-16 11:12:49 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateAuditComponent } from './pages/create-audit/create-audit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'audit',
+    component: CreateAuditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/interfaces/enterprise.ts
@@ -0,0 +1,2 @@
+export interface Enterprise {
+}
