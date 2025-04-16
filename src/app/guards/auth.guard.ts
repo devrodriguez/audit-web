b65c0c975d0de252b80b11be6484975e97747e51
John Rodriguez <john.rodriguez.25@hotmail.com>
2025-04-15 19:10:27 -0500

    upgrade angular core

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 import { Observable } from 'rxjs';
 
@@ -9,7 +9,7 @@ import { UserService } from '../services/user.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   appUser: AppUser = {} as AppUser
   pathIgnore: string[] = ['dashboard']
 
